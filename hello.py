d07edbce1c2bdee9d2c4b6e1041c488f89dfeb43
ragamroll <ragamroll@gmail.com>
2024-04-04 14:25:40 -0700

    Hide header

--- hello.py
+++ hello.py
@@ -3,6 +3,7 @@ import streamlit as st
 hide_menu_style = """
         <style>
         #MainMenu {visibility: hidden;}
+        header[data-testid="stHeader"] {visibility: hidden;}
         </style>
         """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
