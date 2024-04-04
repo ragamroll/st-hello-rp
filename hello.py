a7800b9157c6d3ebbff2b18505c54b8367aa2216
ragamroll <ragamroll@gmail.com>
2024-04-04 16:38:14 -0700

    Improved login form

--- hello.py
+++ hello.py
@@ -8,18 +8,26 @@ hide_menu_style = """
         """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
 
-st.header('This is a header with a divider', divider='rainbow')
-st.header('_Streamlit_ is :blue[cool] :sunglasses:')
+placeholder = st.empty()
 
-st.write("Hello RP")
+actual_email = "email"
+actual_password = "password"
 
-if 'user' not in st.session_state:
-        with st.form("my_form"):
-                username = st.text_input("Username")
-                password = st.text_input("Password")
-                st.form_submit_button("Login")
-        st.session_state['user'] = username
-        st.session_state['pass'] = password
+# Insert a form in the container
+with placeholder.form("login"):
+    st.markdown("#### Enter your credentials")
+    email = st.text_input("Email")
+    password = st.text_input("Password", type="password")
+    submit = st.form_submit_button("Login")
+
+if submit and email == actual_email and password == actual_password:
+    # If the form is submitted and the email and password are correct,
+    # clear the form/container and display a success message
+    placeholder.empty()
+    #st.success("Login successful")
+elif submit and email != actual_email and password != actual_password:
+    st.error("Login failed")
 else:
-        st.write("session state user")
-        st.write(st.session_state['user'])
+    pass
+
+st.write("Hello")
