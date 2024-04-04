02a22c9fa9923a7c9d6b39c93ec5e3afa2a5efc1
ragamroll <ragamroll@gmail.com>
2024-04-04 14:41:13 -0700

    Add form and session state

--- hello.py
+++ hello.py
@@ -12,3 +12,15 @@ st.header('This is a header with a divider', divider='rainbow')
 st.header('_Streamlit_ is :blue[cool] :sunglasses:')
 
 st.write("Hello RP")
+
+with st.form(key="my_form"):
+  username = st.text_input("Username")
+  password = st.text_input("Password")
+  st.form_submit_button("Login")
+
+if 'user' not in st.session_state:
+    st.session_state['user'] = username
+if 'user' not in st.session_state:
+    st.session_state['pass'] = password
+
+st.write(st.session_state.user)
