ac9d07b85fc756848be81dd2694a45b5edb7cb59
ragamroll <ragamroll@gmail.com>
2024-04-06 06:30:02 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,60 +1,59 @@
+from typing import Literal
+
 import streamlit as st
 
-hide_menu_style = """
-        <style>
-        #MainMenu {visibility: hidden;}
-        header[data-testid="stHeader"] {visibility: hidden;}
-        </style>
-        """
-st.markdown(hide_menu_style, unsafe_allow_html=True)
-# streamlit_app.py
+MARGINS = {
+    "top": "2.875rem",
+    "bottom": "0",
+}
 
-import hmac
-import streamlit as st
+STICKY_CONTAINER_HTML = """
+<style>
+div[data-testid="stVerticalBlock"] div:has(div.fixed-header-{i}) {{
+    position: sticky;
+    {position}: {margin};
+    background-color: white;
+    z-index: 999;
+}}
+</style>
+<div class='fixed-header-{i}'/>
+""".strip()
 
-
-def check_password():
-    """Returns `True` if the user had a correct password."""
-
-    def login_form():
-        """Form with widgets to collect user information"""
-        with st.form("Credentials"):
-            st.text_input("Username", key="username")
-            st.text_input("Password", type="password", key="password")
-            st.form_submit_button("Log in", on_click=password_entered)
-
-    def password_entered():
-        """Checks whether a password entered by the user is correct."""
-        if st.session_state["username"] in st.secrets[
-            "passwords"
-        ] and hmac.compare_digest(
-            st.session_state["password"],
-            st.secrets.passwords[st.session_state["username"]],
-        ):
-            st.session_state["password_correct"] = True
-            #del st.session_state["password"]  # Don't store the username or password.
-            #el st.session_state["username"]
-        else:
-            st.session_state["password_correct"] = False
-
-    # Return True if the username + password is validated.
-    if st.session_state.get("password_correct", False):
-        return True
-
-    # Show inputs for username + password.
-    login_form()
-    if "password_correct" in st.session_state:
-        st.error("😕 User not known or password incorrect")
-    return False
+# Not to apply the same style to multiple containers
+count = 0
 
 
-if not check_password():
-    st.stop()
+def sticky_container(
+    *,
+    height: int | None = None,
+    border: bool | None = None,
+    mode: Literal["top", "bottom"] = "top",
+    margin: str | None = None,
+):
+    if margin is None:
+        margin = MARGINS[mode]
 
-# Main Streamlit app starts here
-st.write("Here goes your normal Streamlit app...")
-st.button("Click me")
-st.session_state["username"]
-st.session_state["password"]
-st.secrets.server[st.session_state["username"]]
+    global count
+    html_code = STICKY_CONTAINER_HTML.format(position=mode, margin=margin, i=count)
+    count += 1
 
+    container = st.container(height=height, border=border)
+    container.markdown(html_code, unsafe_allow_html=True)
+    return container
+
+
+if __name__ == "__main__":
+    st.title("Sticky Container")
+    st.write("This is a demonstration of a sticky container.")
+    for i in range(30):
+        st.write(f"Line {i}")
+    with sticky_container(mode="top", border=True):
+        st.write("This is a sticky container at the top.")
+        st.write("This is a sticky container at the top.")
+    for i in range(30):
+        st.write(f"Line {i}")
+    with sticky_container(mode="bottom", border=True):
+        st.write("This is a sticky container at the bottom.")
+        st.write("This is a sticky container at the bottom.")
+    for i in range(30):
+        st.write(f"Line {i}")
